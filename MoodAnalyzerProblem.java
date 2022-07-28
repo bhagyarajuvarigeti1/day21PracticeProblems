f6b53f3d82198b5f67541c6be39d3bd1550bbdb5
bhagyarajuvarigeti1 <bhagyarajuvarigeti@gmail.com>
2022-07-28 21:48:51 +0530

    day 21 pratice problems

---
+++ MoodAnalyzerProblem.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+public class MoodAnalyzerProblem {
+
+    @Test
+    void sprint(){
+        MoodAnalyzerProblem moodAnalyzerProblem = new MoodAnalyzerProblem();
+        System.out.println("nothing");
+    }
+}
